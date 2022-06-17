cc7f1441a74a2c730f71c5420a5dd4a840c61d1b
renry2 <ryan.ren@oocl.com>
2022-06-17 09:19:50 +0800

    feat: Implementing Time Travel

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -50,12 +50,13 @@ function Square(props) {
           history: [{
             squares: Array(9).fill(null),
           }],
+          stepNumber: 0,
           xIsNext: true,
         };
     }
 
-    handleClick(i) {
-      const history = this.state.history;
+    handleClick(i) {    
+      const history = this.state.history.slice(0, this.state.stepNumber + 1);
       const current = history[history.length - 1];
       const squares = current.squares.slice();
       if (calculateWinner(squares) || squares[i]) {
@@ -66,13 +67,21 @@ function Square(props) {
         history: history.concat([{
           squares: squares,
         }]),
+        stepNumber: history.length,
         xIsNext: !this.state.xIsNext,
+      });
+    }
+
+    jumpTo(step) {
+      this.setState({
+        stepNumber: step,
+        xIsNext: (step % 2) === 0,
       });
     }
 
     render() {
         const history = this.state.history;
-        const current = history[history.length - 1];
+        const current = history[this.state.stepNumber];
         const winner = calculateWinner(current.squares);
         let status;
         if (winner) {
@@ -80,6 +89,17 @@ function Square(props) {
         } else {
           status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
         }
+
+        const moves = history.map((step, move) => {
+          const desc = move ?
+            'Go to move #' + move :
+            'Go to game start';
+          return (
+            <li key={move}>
+              <button onClick={() => this.jumpTo(move)}>{desc}</button>
+            </li>
+          );
+        });
 
         return (
             <div className="game">
@@ -91,7 +111,7 @@ function Square(props) {
             </div>
             <div className="game-info">
                 <div>{status}</div>
-                <ol>{/* TODO */}</ol>
+                <ol>{moves}</ol>
             </div>
             </div>
         );
